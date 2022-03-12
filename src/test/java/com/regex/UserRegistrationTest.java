f239c66a2c40cb302df76eab1c93df7d485900b5
parvez050 <parvezhm050@gmail.com>
2022-03-12 08:42:37 +0530

    Enter a valid First Name

--- src/test/java/com/regex/UserRegistrationTest.java
+++ src/test/java/com/regex/UserRegistrationTest.java
@@ -1,15 +1,13 @@
 package com.regex;
 
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.testng.annotations.Test;
 
-
 public class UserRegistrationTest
 {
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+        @Test
+        public void shouldAnswerWithTrue()
+        {
+            assertTrue( true );
+        }
 }
